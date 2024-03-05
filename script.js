b1728575967c5784aa8df95766c458d4f67af84d
roshan-77 <roshan.khadka.u@gmail.com>
2024-03-05 23:28:08 +1100

    addeded digital clock as well

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var hrsElem = document.querySelector(".hrs")
 var minElem = document.querySelector(".min")
 var secElem = document.querySelector(".sec")
+var digitalClock = document.querySelector(".digital-clock")
+
 
 setInterval(() => {
     clockRunning()
@@ -12,11 +14,14 @@ function clockRunning(){
     var minutes = date.getMinutes()
     var seconds = date.getSeconds()
     
+    var am_or_pm = hours>11?"PM":"AM";
     hours = hours>12?hours-12:hours;
-    
+    seconds = seconds<10?`0${seconds}`:seconds;
+
     hrsElem.style.transform = `rotate(${hours*30}deg)`
     minElem.style.transform = `rotate(${(minutes)*6}deg)`
     secElem.style.transform = `rotate(${seconds*6}deg)`
+    digitalClock.textContent = `${hours}:${minutes}:${seconds} ${am_or_pm}`;
     
 }
 
